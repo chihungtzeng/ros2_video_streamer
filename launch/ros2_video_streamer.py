80862b807fe4930bd6d20ae73af0e0cddb99d062
clark <chihung.tzeng@gmail.com>
2025-11-02 20:30:50 +0800

    update

--- launch/ros2_video_streamer.py
+++ launch/ros2_video_streamer.py
@@ -4,7 +4,6 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    """Generate launch description with multiple components."""
     nodes = []
 
     params = [{"video_path": os.path.join(os.environ["HOME"], "Videos", "demo.mp4"),
